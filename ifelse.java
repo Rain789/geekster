839dd51d5e4d775ccf51582861da083fb429c2b4
Rain789 <69694523+Rain789@users.noreply.github.com>
2021-10-16 21:08:44 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,13 @@
+public class ifelse {
+        public static void main(String[] args) {
+            int num1 = 246736;
+            int num2 = 857746;
+
+            if(num1 == num2) { //200, 100
+                System.out.println("Equal");
+            }else {
+                System.out.println("Not Equal");
+            }
+        }
+
+    }
