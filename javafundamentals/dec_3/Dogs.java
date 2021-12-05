7caceb7eeb5be750dcb338330c8a0973731a29e9
Rain789 <69694523+Rain789@users.noreply.github.com>
2021-12-05 16:56:34 +0530

    Add files via upload

---
+++ javafundamentals/dec_3/Dogs.java
@@ -0,0 +1,25 @@
+//Edit this code so the class Beagle is a subclass of the Dog class. When you run the code it should print “woof!” and then “arf arf”
+public class Dogs
+{
+    public void speak()
+    {
+        System.out.println("woof!");
+    }
+
+    public static void main(String[] args)
+    {
+        Dogs d = new Dogs();
+        d.speak();
+        Dogs b = new Beagle();
+        b.speak();
+    }
+}
+
+class Beagle extends Dogs
+{
+    @Override
+    public void speak()
+    {
+        System.out.println("arf arf");
+    }
+}
