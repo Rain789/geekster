78226e58da75661708399c72a4b2f5bca3a8ebe3
Rain789 <69694523+Rain789@users.noreply.github.com>
2021-12-05 17:01:47 +0530

    Add files via upload

---
+++ javafundamentals/Dec_4/Sport.java
@@ -0,0 +1,33 @@
+/* When a subclass inherits from a superclass, it also inherits its methods; 
+however, it can also override the superclass methods (as well as declare and implement new ones). Consider the following Sports class:*/
+class Sports{
+    String getName(){
+        return "Generic Sports";
+    }
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName() );
+    }
+}
+class Soccer extends Sports{
+
+    @Override
+    String getName(){
+        return "Soccer Class";
+    }
+    void getNumberOfTeamMembers(int n){
+        System.out.println( "Each team has " + n +  " players in " + getName() );
+    }
+}
+
+public class Sport {
+    public static void main(String args[]){
+        Sports obj = new Sports();
+        System.out.println(obj.getName());
+        obj.getNumberOfTeamMembers();
+
+        Soccer obj1 = new Soccer();
+        System.out.println(obj1.getName());
+        obj1.getNumberOfTeamMembers(11);
+    }
+    
+}
---
+++ javafundamentals/Dec_4/Test2.java
@@ -0,0 +1,16 @@
+//Write a method that overloads the talk method by taking in a name and printing “Hello” with that name.
+public class Test2 {
+    public static void talk()
+    {
+          System.out.println("hello there!");
+    }
+
+    public static void talk(String name){
+        System.out.println("hello "+name);
+    }
+
+    public static void main(String[] args)
+    {
+          talk("Matthew");
+    }
+}
